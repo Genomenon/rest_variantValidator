6dfb59068866a6b50498a40654441667341296b6
Clayton Wheeler <cwheeler@genomenon.com>
2025-07-08 17:43:53 -0500

    FIX: object pool init

--- rest_VariantValidator/utils/object_pool.py
+++ rest_VariantValidator/utils/object_pool.py
@@ -69,7 +69,7 @@ class ObjectPool:
 # Pools will be pre-filled to configurable sizes in app.py.
 vval_object_pool = ObjectPool(Validator, initial_pool_size=0, max_pool_size=10)
 g2t_object_pool = ObjectPool(Validator, initial_pool_size=0, max_pool_size=10)
-simple_variant_formatter_pool = SimpleVariantFormatterPool(initial_pool_size=0, max_pool_size=10)
+simple_variant_formatter_pool = ObjectPool(lambda: simpleVariantFormatter, initial_pool_size=0, max_pool_size=10)
 
 
 # <LICENSE>
