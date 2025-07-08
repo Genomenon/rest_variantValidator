3c6913f4c1bef84968b6d16f4c4402a9c553acd2
Clayton Wheeler <cwheeler@genomenon.com>
2025-07-08 17:01:52 -0500

    XXX disable lovd syntax check

--- rest_VariantValidator/endpoints/variantvalidator_endpoints.py
+++ rest_VariantValidator/endpoints/variantvalidator_endpoints.py
@@ -97,7 +97,7 @@ class VariantValidatorClass(Resource):
 
             try:
                 validate = vval.validate(variant_description, genome_build, select_transcripts,
-                                         transcript_set=transcript_model, lovd_syntax_check=True)
+                                         transcript_set=transcript_model, lovd_syntax_check=False)
                 content = validate.format_as_dict(with_meta=True)
             except Exception as e:
                 return {"error": str(e)}, 500
