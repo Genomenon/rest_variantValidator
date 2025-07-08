0d3718750512d675fbfb446b0bccb8a862635ea3
Clayton Wheeler <cwheeler@genomenon.com>
2025-07-08 16:58:10 -0500

    Ensure object pools are populated for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,13 @@ from rest_VariantValidator.utils.object_pool import (
 
 POOLS = [vval_object_pool, g2t_object_pool, simple_variant_formatter_pool]
 
+@pytest.fixture(scope='session')
+def size_object_pools():
+    for pool in [vval_object_pool, g2t_object_pool, simple_variant_formatter_pool]:
+        pool.ensure(1)
+
 @pytest.fixture(scope='function')
-def check_object_pool_leaks():
+def check_object_pool_leaks(size_object_pools):
     counts = [(pool, len(pool)) for pool in POOLS]
     yield
     assert [(pool, len(pool)) for pool in POOLS] == counts
